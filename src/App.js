00031938907435e1793424880dd783d0fd6e9075
Valtteri Raatikainen <valtteri.raatikainen1@gmail.com>
2023-04-11 09:36:55 +0300

    Created header using AppBar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import PollList from './components/PollList';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+
+
 import './App.css';
 function App() {
   return (
     <div className="App">
-    <PollList/>
+      <Box sx={{ flexGrow: 1 }}>
+
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Kyselyt
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <PollList />
     </div>
   );
 }
