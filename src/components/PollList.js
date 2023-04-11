a85381d86ce6311c5eacf6d07a0ff801ef850268
Valtteri Raatikainen <valtteri.raatikainen1@gmail.com>
2023-04-11 10:19:49 +0300

    Added api fetch for front-end testing purposes

--- src/components/PollList.js
+++ src/components/PollList.js
@@ -1,7 +1,19 @@
 import React, {useState, useEffect} from "react";
 function PollList () {
 
+    const [polls, setPolls] = React.useState([]);
 
+    useEffect(() => fetchData(), []);
+
+    const fetchData = () => {
+        fetch('http://carrestapi.herokuapp.com/cars')
+        .then(response => response.json())
+        .then(data => setPolls(data._embedded.cars))
+        .catch(err => console.error(err))
+    }
+
+
+    
 }
 
 export default PollList;
