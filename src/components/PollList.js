527032cd9a5241d093ecf76019b9b608ea0f2511
attemkinen <atte.makinen1@outlook.com>
2023-04-11 10:38:53 +0300

    added columndefs and aggrid to render api data

--- src/components/PollList.js
+++ src/components/PollList.js
@@ -1,16 +1,44 @@
 import React, {useState, useEffect} from "react";
+import { AgGridReact } from "ag-grid-react";
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-material.css';
+
+
 function PollList () {
 
     const [polls, setPolls] = React.useState([]);
 
-    useEffect(() => fetchData(), []);
+    useEffect(()=>fetchData(), []);
 
-    const fetchData = () => {
-        fetch('http://carrestapi.herokuapp.com/cars')
+    const fetchData = ()=> {
+        fetch("https://carrestapi.herokuapp.com/cars")
         .then(response => response.json())
-        .then(data => setPolls(data._embedded.cars))
+        .then(data =>setPolls(data._embedded.cars))
         .catch(err => console.error(err))
-    }
+      };
+
+    const [columnDefs] = useState([
+        {field: "brand", sortable: true, filter: true, flex: 1,},
+        {field: "model", sortable: true, filter: true, flex:1},
+        {field: "fuel", sortable: true, filter: true, flex:1},
+        {field: "year", sortable: true, filter: true,flex:1},
+        {field: "price", sortable: true, filter: true,flex:1},
+       
+
+        
+    ])
+
+    return (
+        <div className="ag-theme-material"
+      style={{height: 600, width:"100%", margin: "auto"}}>
+
+            <AgGridReact
+        rowData={polls}
+        columnDefs={columnDefs}>
+          
+        </AgGridReact>
+        </div>
+    )
 
 
     
